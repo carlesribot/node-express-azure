ffc9e5d113cba1df637e02019dde6118f4e4df44
carlesribot <62298520+carlesribot@users.noreply.github.com>
2020-05-25 17:02:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const contact = require('./routes/contact');
 
 const app = express();
 
+
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
